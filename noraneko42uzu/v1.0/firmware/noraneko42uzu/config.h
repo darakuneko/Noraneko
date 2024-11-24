cd011bc0a900a8300caf222a19f5bffe1d40dd2d
darakuneko <tvkids@gmail.com>
2024-11-24 18:23:50 +0900

    Update noraneko42uzu config.h, keyboard.json, keymap.c, and 28 more files

---
+++ noraneko42uzu/v1.0/firmware/noraneko42uzu/config.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xAA, 0xBF, 0x87, 0xE0, 0x5F, 0x07, 0x8A, 0xE7}
+
+#define VIAL_UNLOCK_COMBO_ROWS {0, 0}
+#define VIAL_UNLOCK_COMBO_COLS {0, 1}
